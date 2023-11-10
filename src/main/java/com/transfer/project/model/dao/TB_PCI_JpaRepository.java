55acd11c077ff4e1cb29e86816592ef3bea046fb
chanho470 <chanho470@naver.com>
2023-11-10 14:18:07 +0900

    chore : [Transfer-Engine] #comment 필요없는 파일 제거

--- src/main/java/com/transfer/project/model/dao/TB_PCI_JpaRepository.java
+++
@@ -1,9 +0,0 @@
-/*
-package com.transfer.project.model.dao;
-
-import com.transfer.project.model.entity.TB_PCI_Entity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TB_PCI_JpaRepository extends JpaRepository<TB_PCI_Entity,String> {
-}
-*/
