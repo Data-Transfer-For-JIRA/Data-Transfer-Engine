bc9c5764533c2d23dbdc437dbcbab5aad81ec628
chanho470 <chanho470@naver.com>
2023-12-05 19:27:19 +0900

    feat : [Transfer-Engine] #comment 프로젝트 생성 스케줄러 API - 프로젝트 생성까지 구현

--- src/main/java/com/scheduler/project/service/TransferProjectBySchedulerImpl.java
+++ src/main/java/com/scheduler/project/service/TransferProjectBySchedulerImpl.java
@@ -1,11 +1,21 @@
 package com.scheduler.project.service;
 
 import com.account.service.Account;
+import com.transfer.project.model.entity.TB_PJT_BASE_Entity;
+import com.transfer.project.service.TransferProject;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
 @Service("transferProjectByScheduler")
@@ -13,14 +23,26 @@ public class TransferProjectBySchedulerImpl implements TransferProjectBySchedule
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
+    private com.transfer.project.model.dao.TB_PJT_BASE_JpaRepository TB_PJT_BASE_JpaRepository;
+
+    @Autowired
+    private TransferProject transferProject;
+
+    @Autowired
     private Account account;
     public void createProject() throws Exception{
         // 프로젝트 10개 조회
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<TB_PJT_BASE_Entity> page = TB_PJT_BASE_JpaRepository.findAllByMigrateFlagFalseOrderByCreatedDateDesc(pageable);
+        // 조회 대상 프로젝트 생성 및 결과 리턴
+        List<Map<String, String>> result_all = new ArrayList<>();
+        Map<String, String> result = new HashMap<>();
 
-        // 조회 대상 프로젝트 생성
-
-        // 결과 값 리턴
-
+        for(TB_PJT_BASE_Entity project : page){
+            String projectCode = project.getProjectCode();
+            result = transferProject.CreateProjectFromDB(1,projectCode);
+            result_all.add(result);
+        }
         // 해당 결과 로그백 으로 저장
 
     }
