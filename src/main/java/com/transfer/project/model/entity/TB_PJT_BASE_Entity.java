b3339714ad72407172f68b768f9083d611cc6faa
chanho470 <chanho470@naver.com>
2023-12-06 18:35:27 +0900

    refactor : [Transfer-Engine] #comment 프로젝트 생성 스케줄러 API - 프로젝트 필드 추가

--- src/main/java/com/transfer/project/model/entity/TB_PJT_BASE_Entity.java
+++ src/main/java/com/transfer/project/model/entity/TB_PJT_BASE_Entity.java
@@ -31,7 +31,54 @@ public class TB_PJT_BASE_Entity {
     @Column(name = "BS_SYSDATE")
     private Date createdDate;
     // 생성시간
+
     @Column(name = "MJ_FLAG")
     private Boolean migrateFlag;
+    //이관 여부 플레그
+
+    //============================================== 왼쪽 데이터 ============================
+    @Column(name = "BS_CLIENT")
+    private String 고객사;
+    //고객사
+    @Column(name = "BS_CONTRACTOR")
+    private String 계약사;
+    //계약사
+    @Column(name = "BS_LINK")
+    private String 연동;
+    //연동
+    @Column(name = "BS_BUSINESS")
+    private String 영업담당자;
+
+    @Column(name = "BS_CONYN")
+    private Boolean 계약여부;
+
+    @Column(name = "BS_CHKTYPE")
+    private String 점검방식;
+
+    @Column(name = "BS_STEP")
+    private int 프로젝트단계;
+
+    // 연락처
+
+    // 관련 프로젝트
+
+    // 제품
+
+    // 연동
+
+    // 바코드
+
+    // 지원 범위
+
+    // 지원 OS
+
+    // 찌원 BS
+
+    // URL
+
+    // 데이터 파일
+
+    // 모듈 버전
+
 
 }
