ea4111b7931d2de4dd019cc667aefc2ab07b98f3
chanho470 <chanho470@naver.com>
2023-12-17 20:23:56 +0900

    feat : [Transfer-Engine] #comment 대용량 데이터 전송시 stream 처리

--- src/main/java/com/utils/WebClientUtils.java
+++ src/main/java/com/utils/WebClientUtils.java
@@ -1,13 +1,24 @@
 package com.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class WebClientUtils {
@@ -52,6 +63,7 @@ public class WebClientUtils {
                 .bodyToMono(responseType);
     }
 
+
     public static <T> Mono<T> put(WebClient webClient, String uri, Object requestBody, Class<T> responseType) {
 
         return webClient.put()
@@ -68,4 +80,35 @@ public class WebClientUtils {
                 .retrieve()
                 .bodyToMono(responseType);
     }
+    public static <T> Flux<T> postByFlux(WebClient webClient, String uri, Object requestBody, Class<T> responseType) {
+
+        // 리쿼스트 객체를 JSON 타입으로 형변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonRequestBody;
+        try {
+            jsonRequestBody = objectMapper.writeValueAsString(requestBody); // 데이터
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to convert request body to JSON", e);
+        }
+
+        DataBufferFactory bufferFactory = new DefaultDataBufferFactory();
+        Flux<DataBuffer> dataBufferFlux = getBufferFlux(jsonRequestBody, bufferFactory, 1024); // 1kb 단위로 데이터 읽어 데이터 버퍼 스트림 생성
+
+        return webClient.post()
+                .uri(uri)
+                .body(BodyInserters.fromDataBuffers(dataBufferFlux))
+                .retrieve()
+                .bodyToFlux(responseType);
+    }
+
+    private static Flux<DataBuffer> getBufferFlux(String data, DataBufferFactory bufferFactory, int bufferSize) {
+        ByteArrayInputStream bis = new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8)); 
+        // 문자열 data를 UTF-8 인코딩의 바이트 배열로 변환 후 ByteArrayInputStream로 생성(바이트 배열을 순차적으로 읽을수있음)
+        // ByteArrayInputStream 바이트 배열을 입력 스트림으로 처리할 수 있게 해주는 클래스
+        // 자바에서 스트림(Stream)은 데이터를 순차적으로 읽고 쓰기 위한 추상화된 개념으로, InputStream은 바이트 단위로 데이터를 읽어들이는 스트림
+
+        return DataBufferUtils.readInputStream(() -> bis, bufferFactory, bufferSize);
+        //InputStream에서 데이터를 읽어서 bufferSize 크기의 DataBuffer로 나눈 후, 이들을 Flux<DataBuffer>로 반환
+    }
+
 }
