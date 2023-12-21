74c24e5641375e04f0d4d7b85e66858c0b3ee97b
chanho470 <chanho470@naver.com>
2023-12-21 15:38:49 +0900

    refactor : [Transfer-Engine] #comment 로그 저장 방법 수정 - 파일 경로 파라메터로 받음 - 디렉토리 없을 때 생성

--- src/main/java/com/utils/SaveLog.java
+++ src/main/java/com/utils/SaveLog.java
@@ -1,27 +1,42 @@
 package com.utils;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SaveLog {
-        private static final String FILE_DIRECTORY = "C:\\log/"; // 파일이 저장될 디렉토리 경로
+    private static final String FILE_DIRECTORY = "C:\\log\\"; // 파일이 저장될 디렉토리 경로
 
-        public static void projectSchedulerResult(String result,Date date) {
+    public static void SchedulerResult(String path ,String result, Date date) {
         // 현재 날짜를 가져옴
         Date currentDate = new Date();
 
         // 파일 이름에 날짜 포함하여 파일 경로 생성
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-        String fileName = "프로젝트_스케줄러_결과_" + dateFormat.format(currentDate) + ".txt";
-        String filePath = FILE_DIRECTORY + fileName;
+        String fileName = "스케줄러_결과_" + dateFormat.format(currentDate) + ".txt";
+        String filePath = FILE_DIRECTORY +path+"\\"+ fileName;
 
-        result = date.toString()+" --------------------- "+result;
+        result = date.toString()+"=================="+result;
 
         try {
+            File file = new File(filePath);
+
+            // 디렉토리가 존재하지 않으면 새로운 디렉토리 생성
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+                System.out.println("새로운 디렉토리가 생성되었습니다. 디렉토리 경로: " + file.getParentFile().getPath());
+            }
+
+            // 파일이 존재하지 않으면 새로운 파일 생성
+            if (!file.exists()) {
+                file.createNewFile();
+                System.out.println("새로운 파일이 생성되었습니다. 파일 경로: " + filePath);
+            }
+
             // 파일에 결과 저장
-            FileWriter fileWriter = new FileWriter(filePath, true); // true: 파일 끝에 추가
+            FileWriter fileWriter = new FileWriter(file, true); // true: 파일 끝에 추가
             fileWriter.write(result);
             fileWriter.write(System.lineSeparator()); // 줄바꿈
             fileWriter.close();
