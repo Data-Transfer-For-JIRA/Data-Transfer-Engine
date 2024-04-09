34bf4694d701be32fb5f1b18af78bc7557960d73
chanho470 <chanho470@naver.com>
2024-04-09 17:43:56 +0900

    refactor : [Transfer-Engine] #comment 사용자 정보 업데이트 정보 추가 - 24.04.09

--- src/main/java/com/jira/account/service/AccountImpl.java
+++ src/main/java/com/jira/account/service/AccountImpl.java
@@ -90,10 +90,12 @@ public class AccountImpl implements Account {
             TB_JIRA_USER_Entity existUser = TB_JIRA_USER_JpaRepository.findByAccountId(userInfo.getAccountId());
 
             if(existUser != null){ // 정보 조회시 있으면 업데이트
-
+                existUser.setDisplayName(userInfo.getDisplayName());
                 existUser.setAccountId(userInfo.getAccountId());
                 existUser.setEmailAddress(userInfo.getEmailAddress());
                 existUser.setDisplayName(userInfo.getDisplayName());
+                existUser.setTeam(userInfo.getTeamName());
+                existUser.setPart(userInfo.getPart());
                 TB_JIRA_USER_JpaRepository.save(existUser);
 
             }else{ // 없으면 추가
